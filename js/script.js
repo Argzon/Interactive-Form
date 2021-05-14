c731c377cc14052095a34b579d8a804419623a96
Argzon <argzon01@gmail.com>
2021-05-14 15:14:29 +0200

    Completed Job Role Filter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("Test");
+const name = document.getElementById('name');
+name.focus();
+
+const jobRole = document.querySelector('#title');
+const otherJobRole = document.querySelector('#other-job-role');
+
+otherJobRole.style.display = 'none';
+
+jobRole.addEventListener('change', (e) => {
+    if (e.target.value == 'other') {
+        otherJobRole.style.display = 'block';
+    } else {
+        otherJobRole.style.display = 'none';
+    }
+});
