f8f581dbb89b6469d2d57edd691058bbfe948010
Argzon <argzon01@gmail.com>
2021-05-14 22:03:52 +0200

    T-Shit Section Modified

--- js/script.js
+++ js/script.js
@@ -13,3 +13,23 @@ jobRole.addEventListener('change', (e) => {
         otherJobRole.style.display = 'none';
     }
 });
+
+// T-Shirt Info
+const design = document.querySelector('#design');
+const color = document.querySelector('#color');
+const colorChildren = color.children;
+
+color.disabled = true;
+
+design.addEventListener('change', (e) => {
+    color.disabled = false;
+    for ( let i = 1; i < colorChildren.length; i++ ) {
+        const val = e.target.value;
+        const attr = colorChildren[i].getAttribute('data-theme');
+        if (val !== attr) {
+            colorChildren[i].setAttribute('hidden', 'true');
+        } else {
+            colorChildren[i].removeAttribute('hidden');
+        }
+    }
+})
